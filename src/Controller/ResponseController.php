0a23c12b326c6847fdb1642ca5facdf3108ff61a
Vellanos <davidbak38@gmail.com>
2024-04-19 14:43:04 +0200

    create response

--- src/Controller/ResponseController.php
+++ src/Controller/ResponseController.php
@@ -13,6 +13,39 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class ResponseController extends AbstractController
 {
+    #[Route('/thread/{id_thread}/response/new', name: 'app_response_new', priority: 10)]
+    public function createThread(
+        $id_thread,
+        Request $request,
+        EntityManagerInterface $entityManager,
+    ) {
+        $response = new EntityResponse();
+
+        $threadRepository = $entityManager->getRepository(Thread::class);
+        $thread = $threadRepository->find($id_thread);
+
+        $form = $this->createForm(ResponseFormType::class, $response);
+
+        $form->handleRequest($request);
+
+        $user = $this->getUser();
+
+        if ($form->isSubmitted() && $form->isValid() && !empty($user)) {
+            $response->setCreated(new \Datetime);
+            $response->setEdited(new \Datetime);
+            $response->setUser($user);
+            $response->setThread($thread);
+
+            $entityManager->persist($response);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_thread', ['id' => $id_thread]);
+        }
+
+        return $this->render('response/create.html.twig', [
+            'createForm' => $form
+        ]);
+    }
+
     #[Route('/thread/{id_thread}/response/{id_response}/edit', name: 'app_response_edit')]
     public function editResponse(
         $id_thread,
